2991c5dc551b28d3eab30a2660b09959f8605299
Andrew Bayer <andrew.bayer@gmail.com>
2016-11-03 11:16:13 -0700

    Actually use an alternate Dockerfile, duh

--- pipeline-model-definition/src/test/java/org/jenkinsci/plugins/pipeline/modeldefinition/AgentTest.java
+++ pipeline-model-definition/src/test/java/org/jenkinsci/plugins/pipeline/modeldefinition/AgentTest.java
@@ -164,6 +164,25 @@ public class AgentTest extends AbstractModelDefTest {
         j.assertLogContains("HI THERE", b);
     }
 
+    @Test
+    public void fromAlternateDockerfile() throws Exception {
+        assumeDocker();
+        // Bind mounting /var on OS X doesn't work at the moment
+        onAllowedOS(PossibleOS.LINUX);
+
+        prepRepoWithJenkinsfile("fromAlternateDockerfile");
+        sampleRepo.write("Dockerfile.alternate", "FROM ubuntu:14.04\n\nRUN echo 'HI THERE' > /hi-there\n\n");
+        sampleRepo.git("add", "Dockerfile.alternate");
+        sampleRepo.git("commit", "--message=Dockerfile");
+
+        WorkflowRun b = getAndStartBuild();
+        j.assertBuildStatusSuccess(j.waitForCompletion(b));
+        j.assertLogContains("[Pipeline] { (foo)", b);
+        j.assertLogContains("The answer is 42", b);
+        j.assertLogContains("-v /tmp:/tmp -p 8000:8000", b);
+        j.assertLogContains("HI THERE", b);
+    }
+
     private WorkflowRun agentDocker(final String jenkinsfile) throws Exception {
         assumeDocker();
         // Bind mounting /var on OS X doesn't work at the moment
