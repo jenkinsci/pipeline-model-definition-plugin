dc0783c659547188004a8e14930765e69aeeeafb
Andrew Bayer <andrew.bayer@gmail.com>
2017-03-13 07:31:27 -0700

    Review comments

--- pipeline-model-api/src/main/java/org/jenkinsci/plugins/pipeline/modeldefinition/DescriptorLookupCache.java
+++ pipeline-model-api/src/main/java/org/jenkinsci/plugins/pipeline/modeldefinition/DescriptorLookupCache.java
@@ -35,10 +35,13 @@ import org.jenkinsci.plugins.structs.SymbolLookup;
 import org.jenkinsci.plugins.structs.describable.DescribableModel;
 import org.jenkinsci.plugins.workflow.steps.Step;
 import org.jenkinsci.plugins.workflow.steps.StepDescriptor;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+@Restricted(NoExternalUse.class)
 @Extension
 public class DescriptorLookupCache {
     private transient Map<String, StepDescriptor> stepMap;
