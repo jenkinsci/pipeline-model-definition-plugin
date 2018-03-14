8353a8ce29e7a4ff6940634ece3fe53af2f628de
Nicolas De Loof <nicolas.deloof@gmail.com>
2018-03-14 14:14:09 +0100

    mark as ExtensionPoint + javadoc
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- pipeline-model-definition/src/main/java/org/jenkinsci/plugins/pipeline/modeldefinition/agent/impl/ContainerAgentProvider.java
+++ pipeline-model-definition/src/main/java/org/jenkinsci/plugins/pipeline/modeldefinition/agent/impl/ContainerAgentProvider.java
@@ -1,12 +1,17 @@
 package org.jenkinsci.plugins.pipeline.modeldefinition.agent.impl;
 
+import hudson.ExtensionPoint;
 import hudson.model.Describable;
 
 /**
- * Provider for ContainerAgent, to adapt agent description to actual container launcher on infrastructure using adequate API
+ * Provider for ContainerAgent, to adapt agent description to actual container launcher on infrastructure using adequate API.
+ * {@link DockerContainerAgentProvider} provides a default implementation for this based on docker-plugin, but other
+ * docker-related plugins (like kubernetes-plugin) can also implement this extension point and offer equivalent
+ * containerized agent provisioning using specific API.
+ *
  * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
  */
-public abstract class ContainerAgentProvider implements Describable<ContainerAgentProvider> {
+public abstract class ContainerAgentProvider implements Describable<ContainerAgentProvider>, ExtensionPoint {
 
 
     public abstract String getScriptClass();
