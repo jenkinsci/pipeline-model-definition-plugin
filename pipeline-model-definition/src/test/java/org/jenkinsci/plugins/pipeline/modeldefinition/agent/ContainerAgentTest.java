8a1bde9265e6c7f287e4e48c24e531b372a8f53a
Nicolas De Loof <nicolas.deloof@gmail.com>
2018-03-14 22:59:39 +0100

    assumeDocker
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- pipeline-model-definition/src/test/java/org/jenkinsci/plugins/pipeline/modeldefinition/agent/ContainerAgentTest.java
+++ pipeline-model-definition/src/test/java/org/jenkinsci/plugins/pipeline/modeldefinition/agent/ContainerAgentTest.java
@@ -10,6 +10,7 @@ public class ContainerAgentTest extends AbstractModelDefTest {
 
     @Test
     public void agent() throws Exception {
+        assumeDocker();
         expect("agentContainer")
                 .logContains("Apache Maven 3.5.0")
                 .go();
