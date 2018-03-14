6bd544434e24ee03b2ec9f1f459e764b64a235bb
Nicolas De Loof <nicolas.deloof@gmail.com>
2018-03-14 16:15:15 +0100

    a test case
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

---
+++ pipeline-model-definition/src/test/java/org/jenkinsci/plugins/pipeline/modeldefinition/agent/ContainerAgentTest.java
@@ -0,0 +1,17 @@
+package org.jenkinsci.plugins.pipeline.modeldefinition.agent;
+
+import org.jenkinsci.plugins.pipeline.modeldefinition.AbstractModelDefTest;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:nicolas.deloof@gmail.com">Nicolas De Loof</a>
+ */
+public class ContainerAgentTest extends AbstractModelDefTest {
+
+    @Test
+    public void agent() throws Exception {
+        expect("agentContainer")
+                .logContains("Apache Maven 3.5.0")
+                .go();
+    }
+}
