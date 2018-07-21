e8d1f5fc20da800940f7d7f760ec344324d2230c
Michael Warnecke <13361@nordakademie.de>
2018-07-21 13:59:20 +0200

    [FIXED JENKINS-51931] Prevent NPE when Conditional has no children

--- pipeline-model-definition/src/main/java/org/jenkinsci/plugins/pipeline/modeldefinition/when/impl/AbstractConditionalWithChildren.java
+++ pipeline-model-definition/src/main/java/org/jenkinsci/plugins/pipeline/modeldefinition/when/impl/AbstractConditionalWithChildren.java
@@ -27,6 +27,8 @@ package org.jenkinsci.plugins.pipeline.modeldefinition.when.impl;
 import org.jenkinsci.plugins.pipeline.modeldefinition.when.DeclarativeStageConditional;
 
 import javax.annotation.Nonnull;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -36,7 +38,7 @@ public abstract class AbstractConditionalWithChildren<C extends AbstractConditio
     private final List<DeclarativeStageConditional<? extends DeclarativeStageConditional>> children;
 
     public AbstractConditionalWithChildren(List<DeclarativeStageConditional<? extends DeclarativeStageConditional>> children) {
-        this.children = children;
+        this.children = children != null ? children : new ArrayList<>();
     }
 
     @Override
