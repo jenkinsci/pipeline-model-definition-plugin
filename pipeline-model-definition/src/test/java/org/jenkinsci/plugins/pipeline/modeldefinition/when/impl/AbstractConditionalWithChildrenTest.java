a4c5afc9933dfcd16f2c7788c910d1a44ef8d0df
Michael Warnecke <13361@nordakademie.de>
2018-08-07 17:30:52 +0200

    Fix indentation

--- pipeline-model-definition/src/test/java/org/jenkinsci/plugins/pipeline/modeldefinition/when/impl/AbstractConditionalWithChildrenTest.java
+++ pipeline-model-definition/src/test/java/org/jenkinsci/plugins/pipeline/modeldefinition/when/impl/AbstractConditionalWithChildrenTest.java
@@ -10,19 +10,21 @@ import org.junit.Test;
 import org.jvnet.hudson.test.Issue;
 
 public class AbstractConditionalWithChildrenTest {
-	
-    @Issue("JENKINS-51931")
-    @Test
-    public void conditionalWithChildrenNotNull() {
-    	AbstractConditionalWithChildren<AllOfConditional> allOfConditional = new AllOfConditional(null);
-    	List<DeclarativeStageConditional<? extends DeclarativeStageConditional>> allOfChildren = allOfConditional.getChildren();
+
+	@Issue("JENKINS-51931")
+	@Test
+	public void conditionalWithChildrenNotNull() {
+		AbstractConditionalWithChildren<AllOfConditional> allOfConditional = new AllOfConditional(null);
+		List<DeclarativeStageConditional<? extends DeclarativeStageConditional>> allOfChildren = allOfConditional
+				.getChildren();
 		assertNotNull(allOfChildren);
-    	assertTrue(allOfChildren.isEmpty());
-    	
-    	AbstractConditionalWithChildren<AnyOfConditional> anyOfConditional = new AnyOfConditional(null);
-    	List<DeclarativeStageConditional<? extends DeclarativeStageConditional>> anyOfChildren = anyOfConditional.getChildren();
+		assertTrue(allOfChildren.isEmpty());
+
+		AbstractConditionalWithChildren<AnyOfConditional> anyOfConditional = new AnyOfConditional(null);
+		List<DeclarativeStageConditional<? extends DeclarativeStageConditional>> anyOfChildren = anyOfConditional
+				.getChildren();
 		assertNotNull(anyOfChildren);
-    	assertTrue(anyOfChildren.isEmpty());
-    }
+		assertTrue(anyOfChildren.isEmpty());
+	}
 
 }
