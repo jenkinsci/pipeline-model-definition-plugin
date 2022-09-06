2b1fdc4a7c25efbdd72007f58c0edfbd724706f1
Alexander Brandes <mc.cache@web.de>
2022-09-07 00:12:43 +0200

    feat: Refresh sidebar icon

--- pipeline-model-definition/src/main/java/org/jenkinsci/plugins/pipeline/modeldefinition/actions/RestartDeclarativePipelineAction.java
+++ pipeline-model-definition/src/main/java/org/jenkinsci/plugins/pipeline/modeldefinition/actions/RestartDeclarativePipelineAction.java
@@ -74,7 +74,7 @@ public class RestartDeclarativePipelineAction implements Action {
     }
 
     @Override public String getIconFileName() {
-        return isRestartEnabled() ? "plugin/pipeline-model-definition/images/24x24/restart-stage.png" : null;
+        return isRestartEnabled() ? "plugin/pipeline-model-definition/images/svgs/restart-stage.svg" : null;
     }
 
     @Override public String getUrlName() {
